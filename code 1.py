0c9e3f2284dc0aee86e6cd113c9edb17854dc989
shantanu nimkar <59206895+shantanu1905@users.noreply.github.com>
2020-03-11 21:40:02 +0530

    Add files via upload

---
+++ code 1.py
@@ -0,0 +1,7 @@
+sum =0
+list=[11,22,33,44,55,66,77]
+for num in list:
+    sum =sum +num
+    average=sum /len(list)
+    print("sum oflistelementis:",sum)
+    print("Averageoflistelementis",average)
---
+++ code 2.py
@@ -0,0 +1,7 @@
+n=int(input("Enternumber:"))
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+    print("Reverseofthenumber:",rev)

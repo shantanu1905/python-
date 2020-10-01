c805f9d9ba0fe3b56787764127a33fca9ad87d6a
shubnimkar <46809421+shubnimkar@users.noreply.github.com>
2020-10-01 12:39:21 +0530

    Update get Host IP addresss.py

--- get Host IP addresss.py
+++ get Host IP addresss.py
@@ -1,8 +1,13 @@
+# Import socket 
 import socket
-print("wank to get ipaddress (y/n)?");
-check = input();
-if check == "n":
-    exit();
-else:
-    print("\n Your IP address is : ",end="");
-    print(socket.gethostbyname(socket.gethostname()));
+
+# To get the hostname by socket.gethostname() 
+hostname = socket.gethostname()
+
+# Getting the IP address using socket.gethostbyname() 
+ip_address = socket.gethostbyname(hostname)
+
+# Printing the hostname and ip_address
+
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")

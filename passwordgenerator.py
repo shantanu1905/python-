3eaf5be09e523ff6e1968517bcc4a98487ecff56
shantanu nimkar <59206895+shantanu1905@users.noreply.github.com>
2020-11-18 18:27:12 +0530

    update in code
    #get_random_alphanumeric_string(8)
    #get_random_alphanumeric_string(8)

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -7,5 +7,5 @@ def get_random_alphanumeric_string(length):
     result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
     print("Random alphanumeric String is:", result_str)
 
-get_random_alphanumeric_string(8)
-get_random_alphanumeric_string(8)
+#get_random_alphanumeric_string(8)
+#get_random_alphanumeric_string(8)

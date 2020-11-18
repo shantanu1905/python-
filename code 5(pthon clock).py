5ca8fd9843b5812c279e05659b69e8c0b267935c
shantanu nimkar <59206895+shantanu1905@users.noreply.github.com>
2020-11-18 17:34:39 +0530

    d

--- code 5(pthon clock).py
+++
@@ -1,6 +0,0 @@
-import time
-while True:
-    from datetime import datetime
-    now = datetime.now()  
-    print ("%s/%s/%s %s:%s:%s" % (now.month,now.day,now.year,now.hour,now.minute,now.second)) 
-    time.sleep(1)
